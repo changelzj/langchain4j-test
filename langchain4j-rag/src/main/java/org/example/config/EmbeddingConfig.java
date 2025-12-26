1251dfe15b4ec815aa08f91d2caa19672e9d1635
liuzijian <t-zijian.liu@pcitc.com>
2025-12-26 09:12:29 +0800

    add

--- langchain4j-rag/src/main/java/org/example/config/EmbeddingConfig.java
+++ langchain4j-rag/src/main/java/org/example/config/EmbeddingConfig.java
@@ -2,10 +2,12 @@ package org.example.config;
 
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 
 @Configurable
 public class EmbeddingConfig {
 
+    @Bean
     public EmbeddingModel embeddingModel() {
         return null;
     }
